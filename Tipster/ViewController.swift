e63436e7d025c81e644f3c27a4744c5ec958c080
edelafuente22 <ofthefountain22@gmail.com>
2017-11-01 17:31:18 -0700

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Edward de la Fuente on 11/1/17.
+//  Copyright © 2017 Edward de la Fuente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
